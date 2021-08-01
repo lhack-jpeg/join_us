e63c8fca87afda122dd3f64e51dd4548ff137097
lhack-jpeg <luke-hackett@outlook.com>
2021-08-01 11:24:30 +1000

    add schema for users to start a database

---
+++ schema.sql
@@ -0,0 +1,15 @@
+-- CREATE DATABASE join_us;
+use join_us;
+
+-- CREATE TABLE users(
+--     email VARCHAR(255) PRIMARY KEY,
+--     created_at TIMESTAMP DEFAULT NOW()
+-- );
+show tables;
+desc users;
+
+-- INSERT INTO users (email) VALUES
+-- ('katie34@yahoo.com'),
+-- ('someone@gmail.com');
+
+SELECT * FROM users;
